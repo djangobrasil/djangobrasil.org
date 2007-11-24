8c975729be32ea75680b6f0a663fa8ae82d43cfc
semente <semente+github@taurinus.org>
2007-11-24 22:09:24 +0000

    Added restructuredtext filter on docs.html template.

--- src/djangobrasil/settings.py
+++ src/djangobrasil/settings.py
@@ -98,6 +98,7 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'django.contrib.comments',    
     'django.contrib.flatpages',
+    'django.contrib.markup',
     'djangobrasil.apps.blog',
     'djangobrasil.apps.aggregator',
 )
