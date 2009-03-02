1d7cc090d6d92eb32f27e053aa9c68bdd21f7889
semente <semente+github@taurinus.org>
2009-03-02 22:35:23 +0000

    Desfeita alteração anterior.

--- bin/update-feeds.sh
+++ bin/update-feeds.sh
@@ -3,7 +3,7 @@
 # File path: /usr/local/bin/update-feeds.sh
 #
 
-SOURCE_DIR="$(pwd)/../src"
+SOURCE_DIR=/srv/webapps/www.djangobrasil.org/online-branch/src
 
 export PYTHONPATH=$SOURCE_DIR:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=djangobrasil.settings
