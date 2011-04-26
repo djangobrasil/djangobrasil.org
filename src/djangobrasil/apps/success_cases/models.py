bdae28ba085711eef49f27191fee0422de4ca5a7
Herman Caldara <hermancaldara@gmail.com>
2011-04-26 12:03:48 -0300

    Melhorada a apresentação do form para cadastro de casos de sucesso

--- src/djangobrasil/apps/success_cases/models.py
+++ src/djangobrasil/apps/success_cases/models.py
@@ -1,13 +1,18 @@
+# coding: utf-8
+
 from django.db import models
 from django.template.defaultfilters import slugify
 
 
 class SuccessCase(models.Model):
 
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    author = models.CharField(max_length=200)
-    email = models.EmailField(max_length=100)
+    title = models.CharField(max_length=100, verbose_name="Título")
+    short_description = models.CharField(
+        max_length=250, verbose_name="Descrição resumida"
+    )
+    long_description = models.TextField(verbose_name="Descrição Completa")
+    author = models.CharField(max_length=200, verbose_name="Autor")
+    email = models.EmailField(max_length=100, verbose_name="Email")
     url = models.URLField(max_length=10000)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     
