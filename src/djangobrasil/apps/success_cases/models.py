db204b2f12f0a7509f75a9a9f037410ccf545228
Herman Caldara <hermancaldara@gmail.com>
2011-04-29 21:06:16 -0300

    Adicionado método __unicode__

--- src/djangobrasil/apps/success_cases/models.py
+++ src/djangobrasil/apps/success_cases/models.py
@@ -16,6 +16,9 @@ class SuccessCase(models.Model):
     url = models.URLField(max_length=10000)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     
+    def __unicode__(self):
+        return self.title
+    
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(SuccessCase, self).save(*args, **kwargs)
