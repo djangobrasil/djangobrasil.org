45e37c147af1ba328aa72272ec28756f1708a8f1
Flavia Missi <flaviamissi@gmail.com>
2011-09-11 11:32:36 -0300

    Changed success url and template used in contact view

--- src/djangobrasil/contact/views.py
+++ src/djangobrasil/contact/views.py
@@ -4,6 +4,6 @@ from contact.forms import ContactForm
 
 
 class ContactView(FormView):
-    template_name = 'contact/contact.html'
+    template_name = 'flatfiles/contato.html'
     form_class = ContactForm
-    success_url = 'contact/'
+    success_url = 'contato/'
