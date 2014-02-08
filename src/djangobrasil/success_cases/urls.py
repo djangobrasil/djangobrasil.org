0737acddf8dc2ac22f8e1782ecc9d12e1c1fb6a2
Andrews Medina <andrewsmedina@gmail.com>
2014-02-08 14:46:05 -0200

    success_cases: pep8 cleanup.

--- src/djangobrasil/success_cases/urls.py
+++ src/djangobrasil/success_cases/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 
-urlpatterns = patterns('success_cases.views',
+urlpatterns = patterns(
+    'success_cases.views',
     (r'^$', 'all_cases'),
     (r'^novo/$', 'new_case'),
     (r'^(?P<slug>[\w_-]+)/$', 'specific_case'),
